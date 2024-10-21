79ccc9bf60d8795b5305dc5ee4a69c100ee043b6
Alexandre Taillet <alexandre.taillet@student.vinci.be>
2024-10-21 12:00:51 +0200

    new: ex-2.3

--- exercices/2.1-2-3-4/src/components/App/index.tsx
+++ exercices/2.1-2-3-4/src/components/App/index.tsx
@@ -4,33 +4,57 @@ import './App.css';
 
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
-  
 
   const cinema1Name = "UGC DeBrouckère";
-  const cinema1Movie1Title = "Film 1 - DeBrouckère";
-  const cinema1Movie1Director = "Director A";
-  const cinema1Movie2Title = "Film 2 - DeBrouckère";
-  const cinema1Movie2Director = "Director B";
-  const cinema1Movies = [
-    { title: cinema1Movie1Title, director: cinema1Movie1Director },
-    { title: cinema1Movie2Title, director: cinema1Movie2Director },
-  ];
 
-  const cinema2Name = "UGC Toison d'Or";
-  const cinema2Movie1Title = "Film 1 - Toison d'Or";
-  const cinema2Movie1Director = "Director C";
-  const cinema2Movie2Title = "Film 2 - Toison d'Or";
-  const cinema2Movie2Director = "Director D";
-  const cinema2Movies = [
-    { title: cinema2Movie1Title, director: cinema2Movie1Director },
-    { title: cinema2Movie2Title, director: cinema2Movie2Director },
-  ];
+  const moviesCinema1 = [
+  {
+    title: "HAIKYU-THE DUMPSTER BATTLE",
+    director: "Susumu Mitsunaka",
+  },
+  {
+    title: "GOODBYE JULIA",
+    director: "Mohamed Kordofani",
+  },
+  {
+    title: "INCEPTION",
+    director: "Christopher Nolan",
+  },
+  {
+    title: "PARASITE",
+    director: "Bong Joon-ho",
+  },
+];
 
+const cinema2Name = "UGC Toison d'Or";
+
+const moviesCinema2 = [
+  {
+    title: "THE WATCHERS",
+    director: "Ishana Night Shyamalan",
+  },
+  {
+    title: "BAD BOYS: RIDE OR DIE",
+    director: "Adil El Arbi, Bilall Fallah",
+  },
+  {
+    title: "TENET",
+    director: "Christopher Nolan",
+  },
+  {
+    title: "THE IRISHMAN",
+    director: "Martin Scorsese",
+  },
+]; 
+
+  
   return (
     <div>
       <PageTitle title={pageTitle} />
-      <Cinema cinemaName={cinema1Name} movies={cinema1Movies} />
-      <Cinema cinemaName={cinema2Name} movies={cinema2Movies} />
+
+      <Cinema cinemaName={cinema1Name} movies= {moviesCinema1} />
+
+      <Cinema cinemaName={cinema2Name} movies={moviesCinema2} />
     </div>
   );
 };
